ec3ff92c2ee6437c685da2c0fff40fae5080070a
Eduardo Grajeda <tatofoo@gmail.com>
2013-10-12 17:22:04 -0300

    updated structure

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,13 @@
-angular.module('ngBoilerplate', [
+angular.module('fobu', [
   'templates-app',
   'templates-common',
-  'ngBoilerplate.editor',
+  'fobu.home',
   'ui.state',
   'ui.route'
 ])
 
 .config(function myAppConfig($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/editor');
+  $urlRouterProvider.otherwise('/');
 })
 
 .run(function run() {
@@ -15,32 +15,3 @@ angular.module('ngBoilerplate', [
 })
 
 ;
-
-/*
-angular.module( 'ngBoilerplate', [
-  'templates-app',
-  'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
-  'ui.state',
-  'ui.route'
-])
-
-.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
-})
-
-.run( function run () {
-})
-
-.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
-  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
-    if ( angular.isDefined( toState.data.pageTitle ) ) {
-      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
-    }
-  });
-})
-
-;
-
-*/
--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,4 @@
-angular.module('ngBoilerplate.editor-config', [])
+angular.module('fobu.config', [])
 
 .factory('config', function() {
   return {
