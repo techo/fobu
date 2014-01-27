b17ebe136c4efaba9317270b545514795b0afe90
Eduardo Grajeda <tatofoo@gmail.com>
2014-01-27 17:40:06 -0300

    added working nested forms code

--- src/lib/view/view.js
+++ src/lib/view/view.js
@@ -4,19 +4,42 @@ angular.module('fobu.view', [
   'fobu.templates-common',
   'fobu.resources.form',
   'fobu.services.elementTransformer',
+  'fobu.services.elementFiller',
   'fobu.directives.formElementRenderer',
   'fobu.filters.inColumn'
 ])
 
-.controller('FobuViewCtrl', function($scope, $stateParams, Form, config, elementTransformer) {
-  var formId = $stateParams.formId;
-  if ($stateParams.nestedFormId) {
-    formId = $stateParams.nestedFormId;
+.controller('FobuViewCtrl', function($scope, $stateParams, Form, fobuConfig, elementTransformer, elementFiller) {
+  if (! $stateParams.formId) {
+    return;
   }
 
-  $scope.form = Form.get({ formId: formId }, function() {
-    $scope.form = elementTransformer.transformRecursively($scope.form, config);
+  var form = Form.get({ formId: $stateParams.formId }, function() {
+    form = elementTransformer.transformRecursively(form, fobuConfig);
   });
+
+  if ($stateParams.nestedFormId) {
+    form.$promise.then(function() {
+      var nestedFormElement = findElementById(form.elements, $stateParams.nestedFormId);
+      if (nestedFormElement) {
+        $scope.form = Form.get({ formId: nestedFormElement.formId }, function() {
+          $scope.form = elementTransformer.transformRecursively($scope.form, fobuConfig);
+        });
+      }
+    });
+  } else {
+    form.$promise.then(function() {
+      $scope.form = form;
+    });
+  }
+
+  function findElementById(elements, id) {
+    for (var i = 0; i < elements.length; i++) {
+      if (elements[i].id == id) {
+        return elements[i];
+      }
+    }
+  }
 })
 
 ;
