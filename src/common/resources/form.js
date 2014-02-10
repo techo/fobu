4cecdc339d06066ce1d3dbe23859de822551b217
Eduardo Grajeda <tatofoo@gmail.com>
2014-02-10 11:01:16 -0300

    changed Form.fill doesn't empty form data by default

--- src/common/resources/form.js
+++ src/common/resources/form.js
@@ -14,16 +14,20 @@ angular.module('fobu.resources.form', [
     }
   });
 
-  Form.prototype.fill = function(data) {
-    fill(this, data);
+  Form.prototype.fill = function(data, overwrite) {
+    fill(this, data, overwrite || false);
   };
 
-  function fill(element, data) {
-    element.value = data[element.name];
+  function fill(element, data, overwrite) {
+    if (overwrite) {
+      element.value = data[element.name];
+    } else {
+      element.value = data[element.name] || element.value;
+    }
 
     if ('elements' in element) {
       for (var i = 0; i < element.elements.length; i++) {
-        fill(element.elements[i], data);
+        fill(element.elements[i], data, overwrite);
       }
     }
   }
