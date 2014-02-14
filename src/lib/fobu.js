7384386922156a7c37a7f160df6fecdbf4b3030a
Eduardo Grajeda <tatofoo@gmail.com>
2014-02-14 10:36:04 -0300

    added more primary dependencies to 'fobu' module, to help projects that use it

--- src/lib/fobu.js
+++ src/lib/fobu.js
@@ -1,6 +1,9 @@
 angular.module('fobu', [
   'fobu.config',
+  'fobu.templates-app',
+  'fobu.templates-common',
   'fobu.directives.formElementRenderer',
+  'fobu.services.elementTransformer',
   'fobu.resources.form'
 ])
 
